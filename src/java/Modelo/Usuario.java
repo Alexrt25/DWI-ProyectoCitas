f7f9097038aaaa086a5a4ad87d5f2fe36dd37250
Erpro-prog <136927158+Erpro-prog@users.noreply.github.com>
2023-06-19 22:37:20 -0500

    Add files via upload
    update files

---
+++ src/java/Modelo/Usuario.java
@@ -0,0 +1,98 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Modelo;
+
+/**
+ *
+ * @author Grupo 5
+ */
+public class Usuario {
+    int idUsuario;
+    String nombre;
+    String correo;
+    String contrasenia;
+    int estadoUsuario;
+    int idPerfil;
+    String nombrePerfil;
+
+    public Usuario() {
+    }
+
+    public Usuario(int idUsuario, String nombre, String correo, String contrasenia, int estadoUsuario, int idPerfil) {
+        this.idUsuario = idUsuario;
+        this.nombre = nombre;
+        this.correo = correo;
+        this.contrasenia = contrasenia;
+        this.estadoUsuario = estadoUsuario;
+        this.idPerfil = idPerfil;
+    }
+
+      public String getNombrePerfil() {
+        int perfil = this.getIdPerfil();
+        if (perfil == 1) {
+            this.nombrePerfil = "Administrador";
+        } else if (perfil == 2) {
+            this.nombrePerfil = "Asesor";
+        } else {
+            this.nombrePerfil = "Cliente";
+        }
+        return nombrePerfil;
+    }
+
+    public void setNombrePerfil(String nombrePerfil) {
+        this.nombrePerfil = nombrePerfil;
+    }
+
+    public int getIdUsuario() {
+        return idUsuario;
+    }
+
+    public void setIdUsuario(int idUsuario) {
+        this.idUsuario = idUsuario;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getContrasenia() {
+        return contrasenia;
+    }
+
+    public void setContrasenia(String contrasenia) {
+        this.contrasenia = contrasenia;
+    }
+
+    public int getEstadoUsuario() {
+        return estadoUsuario;
+    }
+
+    public void setEstadoUsuario(int estadoUsuario) {
+        this.estadoUsuario = estadoUsuario;
+    }
+
+    public int getIdPerfil() {
+        return idPerfil;
+    }
+
+    public void setIdPerfil(int idPerfil) {
+        this.idPerfil = idPerfil;
+    }
+    
+    
+}
+
---
+++ src/java/Modelo/UsuarioAsesor.java
@@ -0,0 +1,93 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Modelo;
+
+/**
+ *
+ * @author Grupo 5
+ */
+public class UsuarioAsesor {
+    
+        Integer IdAsesor;
+        String DocumentoIdentidad;
+        String Telefono;
+        String nombre;
+        String correo;
+        Integer estadoUsuario;
+        String estadoUsuarioNombre;
+        
+public UsuarioAsesor() {
+    
+    }
+
+    public UsuarioAsesor(Integer IdAsesor,String DocumentoIdentidad, String Telefono, String nombre, String correo,Integer estadoUsuario,String estadoUsuarioNombre) {
+        this.IdAsesor = IdAsesor;
+        this.DocumentoIdentidad = DocumentoIdentidad;
+        this.Telefono = Telefono;
+        this.nombre = nombre;
+        this.correo = correo;
+        this.estadoUsuario = estadoUsuario;
+        this.estadoUsuarioNombre = estadoUsuarioNombre;
+    }
+
+    public Integer getIdAsesor() {
+        return IdAsesor;
+    }
+
+    public void setIdAsesor(Integer IdAsesor) {
+        this.IdAsesor = IdAsesor;
+    }
+
+    public String getDocumentoIdentidad() {
+        return DocumentoIdentidad;
+    }
+
+    public String getTelefono() {
+        return Telefono;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setDocumentoIdentidad(String DocumentoIdentidad) {
+        this.DocumentoIdentidad = DocumentoIdentidad;
+    }
+
+    public void setTelefono(String Telefono) {
+        this.Telefono = Telefono;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public Integer getEstadoUsuario() {
+        return estadoUsuario;
+    }
+
+    public void setEstadoUsuario(Integer estadoUsuario) {
+        this.estadoUsuario = estadoUsuario;
+    }
+
+    public String getEstadoUsuarioNombre() {
+        return estadoUsuarioNombre;
+    }
+
+    public void setEstadoUsuarioNombre(String estadoUsuarioNombre) {
+        this.estadoUsuarioNombre = estadoUsuarioNombre;
+    }
+        
+        
+        
+}
